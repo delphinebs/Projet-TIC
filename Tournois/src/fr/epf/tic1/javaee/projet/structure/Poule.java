3d225fd44e126b7ec19901613e93e918b42bebdc
ElBritish <william.bruneau@epfedu.fr>
2015-01-28 14:57:10 +0100

    Modif Test

--- Tournois/src/fr/epf/tic1/javaee/projet/structure/Poule.java
+++ Tournois/src/fr/epf/tic1/javaee/projet/structure/Poule.java
@@ -1,7 +1,11 @@
 package fr.epf.tic1.javaee.projet.structure;
 
+import java.util.ArrayList;
+
 public class Poule {
 
+	private ArrayList<Match> Matchs;
+	
 	public Poule() {
 		// TODO Auto-generated constructor stub
 	}
